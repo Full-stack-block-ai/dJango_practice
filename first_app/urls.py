679d8f5b5fa5213fed156ad7e02cd9da510b3c80
Steve <fishesjourney@gmail.com>
2025-07-27 15:20:00 +0100

    added a dynamic routing example

--- first_app/urls.py
+++ first_app/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     #connects to app
     #routing so empty string is default , views , string name
-    path('', views.index)
+    path('', views.index),
+    #Dynamic path routing to the news feed view
+    path('<topic>', views.newsfeed)
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -2,8 +2,16 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 # Create your views here.
-
+news = {
+    'blog_page': 'Blog page',
+    'media_page': 'Media page'
+}
 #takes in a request to check out the view and returns a http response
 #from from django.http import HttpResponse 
 def index(request):
     return HttpResponse("hello this is my first view inside my_app")
+
+
+# dynamic routing example 
+def newsfeed(request, topic):
+    return HttpResponse( news[topic])
